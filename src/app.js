51f539c126be9a92c20b5fa6db36edb836a91fc1
Elena Sufieva <fiolent06@gmail.com>
2017-07-17 12:44:16 +0200

    Add App component and redux setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import styles from './layout.scss';
+import App from './components/App';
+import store from './store';
 
-render(<div>React route planner</div>, document.getElementById('app'));
+render(<Provider store={store}>
+          <App />
+      </Provider>, document.getElementById('app'));
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './layout.scss';
+
+const App = () => {
+  return (
+    <div>
+      App component goes here.
+    </div>
+  );
+};
+
+export default App;
