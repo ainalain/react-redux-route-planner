efc2105d0cac598544e2d469209758360686c2fb
Elena Sufieva <fiolent06@gmail.com>
2017-07-21 20:44:41 +0200

    Style travelModa icons

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Icon.scss';
 
-const Icon = ({ width = 32 , height = 32, className = 'icon', glyph }) => {
+const Icon = ({ width = 32 , height = 32, glyph }) => {
   return (
     <div className={styles.wrapper}>
-      <svg className={className} width={width} height={height} viewBox={glyph.viewbox}>
+      <svg className={styles.icon} width={width} height={height} viewBox={glyph.viewbox}>
         <use xlinkHref={glyph.symbol} />
       </svg>
     </div>
