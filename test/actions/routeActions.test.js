9ceb69056b5589384c72fa9902734a2987d6888d
Elena Sufieva <fiolent06@gmail.com>
2017-07-21 12:40:16 +0200

    Add new actions tests

--- test/actions/routeActions.test.js
+++ test/actions/routeActions.test.js
@@ -27,18 +27,39 @@ describe('Route actions', () => {
     expect(action).toEqual(expectedAction);
   });
 
+  it('should create an GET_ROUTE_SUCCESS action when route is updated', () => {
+    const expectedAction = {
+      type: types.GET_ROUTE_SUCCESS,
+      payload: {}
+    };
+    const action = actions.updateCurrentRoute({});
+    console.log('action: ', action);
+    expect(action).toEqual(expectedAction);
+  });
+
   //hard case, cannot normally test async action with google service
   describe('Actions woth Google route service', () => {
     const api = mockGoogleAPI({success: true });
-    const params = { google: api };
-    it('should create GET_ROUTE_SUCCESS action when route request has been succeeded', (done) => {
+    it('should create GET_ROUTE_SUCCESS action when route request has been succeeded', () => {
       const expectedAction = { type: types.GET_ROUTE_SUCCESS, payload: response };
 
     const service = api.maps.DirectionsService();
     const action = service.route(true);
 
     expect(action).toEqual(expectedAction);
-    done();
+    });
+
+    it('should create GET_ROUTE_ERROR action when route request has been failed', () => {
+      const googleApi = mockGoogleAPI({success: false });
+      const error = {
+        status: 'UNKNOWN_ERROR',
+        message: ''
+      }
+    const expectedAction = { type: types.GET_ROUTE_ERROR, payload: error };
+    const service = googleApi.maps.DirectionsService();
+    const action = service.route(false);
+
+    expect(action).toEqual(expectedAction);
     });
   });
 });
