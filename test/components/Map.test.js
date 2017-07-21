f330797f101b746a8c22c0dd8d44158ffba198e7
Elena Sufieva <fiolent06@gmail.com>
2017-07-21 13:44:03 +0200

    Add unit tests for connected components

--- test/components/Map.test.js
+++ test/components/Map.test.js
@@ -1,8 +1,13 @@
+import 'jsdom-global/register';
 import expect from 'expect';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import mockGoogleAPI from '../helpers/mockGoogleAPI';
-import { Map } from '../../src/components/Map';
+import ConnectedMap, { Map } from '../../src/components/Map';
+import styles from '../../src/components/Map.scss';
 
 const setup = () => {
   const props = {
@@ -33,10 +38,28 @@ describe('Map with shallow rendering', () => {
   it('calls componentWillReceiveProps method when receives new props', () => {
     const spy = sinon.spy(Map.prototype, 'componentWillReceiveProps');
     const component = setup();
-    global.google = mockGoogleAPI();
+    global.google = mockGoogleAPI({ self: component});
 
     component.setProps({ isScriptLoaded: true, isScriptLoadSucceed:true });
     expect(spy.calledOnce).toBe(true);
     spy.restore();
+  });
+
+  describe('Connected Map test', () => {
+    const mockStore = configureStore([thunk]);
+
+    it('it renders connected component', () => {
+      const store = mockStore({
+        currentRoute: {},
+        savedRoutes: [],
+        error: ''
+      });
+
+      const component = mount(
+        <Provider store={store}>
+          <ConnectedMap />
+        </Provider>)
+      expect(component.find(ConnectedMap).length).toBe(1);
+    });
   });
 });
--- test/components/Marker.test.js
+++ test/components/Marker.test.js
@@ -1,22 +1,42 @@
 import expect from 'expect';
 import React from 'react';
-import { shallow  } from 'enzyme';
+import { shallow, mount  } from 'enzyme';
 import mockGoogleAPI from '../helpers/mockGoogleAPI';
-import { Marker } from '../../src/components/Marker';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import ConnectedMarker, { Marker } from '../../src/components/Marker';
 import styles from '../../src/components/Marker.scss';
 
 describe('Marker with shallow rendering', () => {
+  const props = {
+    map: {},
+    google: mockGoogleAPI(),
+    position: {
+      lat: 50.0614238,
+      lng: 19.9515069
+    }
+  };
   it('always renders a div with correspondant class ', () => {
-    const props = {
-      map: {},
-      google: mockGoogleAPI(),
-      position: {
-        lat: 50.0614238,
-        lng: 19.9515069
-      }
-    };
     const component = shallow(<Marker {...props} />);
     expect(component.find(`div.${styles.marker}`).length).toBe(1);
   });
 
+  describe('Connected Marker test', () => {
+    const mockStore = configureStore([thunk]);
+
+    it('it renders connected component', () => {
+      const store = mockStore({
+        currentRoute: {},
+        savedRoutes: [],
+        error: ''
+      });
+
+      const component = mount(
+        <Provider store={store}>
+          <ConnectedMarker {...props} />
+        </Provider>)
+      expect(component.find(ConnectedMarker).length).toBe(1);
+    });
+  });
 });
